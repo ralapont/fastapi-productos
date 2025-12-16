ccea79e48e3f2c667aae36ae7bb2226089a60188
ralapont <rafael.alapont.gomez@gmail.com>
2025-12-16 22:37:39 +0100

    cors

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from database import init_db, engine, get_session
 from routers import products, categories
+from fastapi.middleware.cors import CORSMiddleware
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -13,5 +14,13 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(title="Microservicio Productos y Categorías", lifespan=lifespan)
 
+# Configuración de CORS 
+app.add_middleware( CORSMiddleware, 
+        allow_origins=["http://localhost:5173"], # origen de tu frontend 
+        allow_credentials=True, 
+        allow_methods=["*"], # GET, POST, PUT, DELETE... 
+        allow_headers=["*"], # Authorization, Content-Type...
+    )
+                   
 app.include_router(products.router)
 app.include_router(categories.router)
